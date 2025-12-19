034840f65079c4fc18b882bcd82a65f2dc2328a3
MD Mostafa Niloy <mostafa@visernic.com>
2025-12-19 20:19:02 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'visernic-manager-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './assets/css/style.css',
+    './assets/js/script.js',
+    './assets/images/visernic-logo.svg',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.31/jspdf.plugin.autotable.min.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate & Remove Old Caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
